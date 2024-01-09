00851308b5f9f587277f36726308d87cc6323127
530465852@qq.com <530465852@qq.com>
2024-01-09 11:30:46 +0800

    支持环境差异配置

--- components.d.ts
+++ components.d.ts
@@ -7,7 +7,6 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    Flow: typeof import('./src/components/Flow/index.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
