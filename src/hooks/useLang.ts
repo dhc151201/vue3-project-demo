6b8e760be16a64852cd56829e544f1487b77e99d
530465852@qq.com <530465852@qq.com>
2024-01-26 14:41:47 +0800

    国际化引入

---
+++ src/hooks/useLang.ts
@@ -0,0 +1,36 @@
+import { computed, type Ref } from "vue"
+import { getCookie, setCookie } from "./useCookie"
+import { useI18n } from "vue-i18n"
+
+const langKey = "lang"
+type lang = 'zh' | 'en'
+
+/**
+ * 获取语言值（优先级：缓存 > 浏览器语言 > 默认语言zh中文）
+ * @returns zh | en
+ */
+export const getLocalLang = (): lang => {
+    return (getCookie(langKey) || {
+        'zh-CN': 'zh',
+        'zh': 'zh',
+        'zh-TW': 'zh',
+        'en-US': 'en',
+    }[navigator.language] || 'zh') as lang
+}
+
+/**
+ * 本地语言设置
+ * @param value 语言值
+ */
+export const setLocalLang = (value: 'zh' | 'en') => {
+    setCookie(langKey, value, 100)
+    document.body.classList.remove("zh")
+    document.body.classList.remove("en")
+    document.body.classList.add(value)
+}
+
+const { locale } = useI18n();
+/**
+ * 计算属性，是否是中文
+ */
+export const isZh:Ref<boolean> = computed(() => locale.value === 'zh')
